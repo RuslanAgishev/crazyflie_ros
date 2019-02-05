a33b48664137670b761c61b990da25fdb0da779e
RuslanAgishev <agishev_ruslan@mail.ru>
2019-02-05 12:32:07 +0300

    swarmskin with timing and recording

--- crazyflie_demo/scripts/swarmskin.py
+++ crazyflie_demo/scripts/swarmskin.py
@@ -9,163 +9,132 @@ import swarmlib
 import message_filters
 from geometry_msgs.msg import PoseStamped, TransformStamped
 import os
+import sys
 from multiprocessing import Process
+from threading import Thread
 import random
+import numpy as np
+from numpy.linalg import norm
+
+
+
 
 def start_recording():
-    os.system("rosbag record -o Desktop/SwarmSkin/ /vicon/crazyflie100/crazyflie100 /vicon/landing_pad/landing_pad")
-def land_detector():
-    while not rospy.is_shutdown():
-        drone1.position()
-        lp1.position()
-        # lp2.position()
-        if abs(drone1.pose[2] - lp1.pose[2])<0.05:
-            print "Stop motors"
-            cf1.stop()
-            # cf1.setParam("ring/effect", 14)
-            time.sleep(0.1)
-            if data_recording:
-                print 'kill the recorder'
-                node_list = os.popen("rosnode list").read()
-                os.system('rosnode kill '+node_list[72:99])
-            print "Shutdown"
-            rospy.signal_shutdown("landed")
-
-
-rospy.init_node('test_high_level')
-
-# Names and variables
-TAKEOFFHEIGHT = 1.8
-data_recording = False
-cf1_name = 'cf1'
-lp1_name = 'lp1'
-lp2_name = 'lp2' # lp1
-
-drone1 = swarmlib.Drone(cf1_name)
-drone_list = [drone1]
-lp1    = swarmlib.Mocap_object(lp1_name)
-lp2    = swarmlib.Mocap_object(lp2_name)
-lp_list = [lp1, lp2]
-
-cf1 = crazyflie.Crazyflie(cf1_name, '/vicon/'+cf1_name+'/'+cf1_name)
-cf_list = [cf1]
-
-# landing_velocity = random.choice([13,22]) #13,22
-# landing_velocity = 13
-# landing_velocity = 22
-landing_velocity = 30
-print "landing_velocity", landing_velocity
-
-
-
-
-
-print "Takeoff"
-for cf in cf_list:
-    cf.setParam("commander/enHighLevel", 1)
-    cf.setParam("stabilizer/estimator", 2) # Use EKF
-    cf.setParam("stabilizer/controller", 2) # Use mellinger controller
-    cf.takeoff(targetHeight = TAKEOFFHEIGHT, duration = 7.0)
-time.sleep(7.0)
-
-
-
-if data_recording:
     print "Data recording started"
-    pose_recording = Process(target=start_recording)
-    pose_recording.start()
+    os.system("mkdir "+Subject_name)
+    os.system("rosbag record -o "+PATH+Subject_name+"/vicon_data /vicon/cf1/cf1 /vicon/cf2/cf2 /vicon/cf3/cf3 /vicon/cf4/cf4 /vicon/lp1/lp1 /vicon/lp2/lp2")
+    # os.system("rosbag record -o "+PATH+"poses /vicon/cf1/cf1 /vicon/cf2/cf2 /vicon/cf3/cf3 /vicon/cf4/cf4 /vicon/lp1/lp1 /vicon/lp2/lp2 /vicon/lp3/lp3 /vicon/lp4/lp4")
 
-print 'Landing...'
-for cf in cf_list:
-    cf.land(targetHeight = 0.0, duration = landing_velocity)
+def land_detector():
+    land_time = - np.ones( min(len(drone_list), len(lp_list)) )
+    while not rospy.is_shutdown():
+        for drone in drone_list: drone.position()
+        for lp in lp_list: lp.position()
+        landed_drones_number = 0
+        for i in range(min(len(drone_list), len(lp_list))):
+            print abs(drone_list[i].pose[2] - lp_list[i].pose[2])
+            if abs(drone_list[i].pose[0] - lp_list[i].pose[0])<0.07 and abs(drone_list[i].pose[1] - lp_list[i].pose[1])<0.07 and abs(drone_list[i].pose[2] - lp_list[i].pose[2])<0.1:
+                landed_drones_number += 1
+                if land_time[i]==-1:
+                    land_time[i] = time.time()-start_time
+                    print("Drone %d is landed after %s seconds" % (i+1, land_time[i]))
+                if toFly:
+                    cf_list[i].stop() # stop motors
+                    cf_list[i].setParam("tf/state", 0) # switch off LEDs
+                time.sleep(0.1)
+        # TODO: stop data recording if all the drones landed
+        # if data_recording and landed_drones_number>0:
+        #     print 'kill the recorder'
+        #     node_list = os.popen("rosnode list").read()
+        #     os.system('rosnode kill '+node_list[72:99])
+        #     print "Number of landed drones: " + str(landed_drones_number)
+        #     if landed_drones_number==len(drone_list): rospy.signal_shutdown("landed")
 
+def goto_land_pose(cf, TakeoffHeight, goalXY):
+    print("Takeoff")
+    cf.takeoff(targetHeight=TakeoffHeight, duration=5.0)
+    time.sleep(5.0)
 
-land_detector()
+    cf.goTo(goal = [goalXY[0], goalXY[1], TakeoffHeight], yaw=0.0, duration=3.0, relative=False)
+    time.sleep(3.0)
 
+    print("Switch on LEDs")
+    cf.setParam("tf/state", 4) # LED is ON
+    time.sleep(1.0)
+    global start_time
+    start_time = time.time()
 
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # cf.goTo(goal = [0.0, -0.4, 0.0], yaw=0.0, duration = 2.0, relative = True)
-    # time.sleep(2.0)
-
-    # cf.goTo(goal = [0.0, 0.8, 0.0], yaw=0.0, duration = 4.0, relative = True)
-    # time.sleep(4.0)
-
-    # cf.goTo(goal = [0.0, -0.4, 0.0], yaw=0.0, duration = 2.0, relative = True)
-    # time.sleep(2.0)
+    print("Descending...")
+    cf.land(targetHeight=-0.1, duration=landing_velocity)
+    time.sleep(landing_velocity)
 
 
+if __name__ == '__main__':
+    rospy.init_node('swarmskin')
+
+    """ initialization """
+    # Names and variables
+    TakeoffHeight  = 1.8
+    data_recording = 0
+    toFly          = 1
+    lp_names = ['lp1', 'lp2']
+    cf_names = ['cf1']
+    # cf_names = ['cf1', 'cf2']
+    # cf_names = ['cf1', 'cf2', 'cf3', 'cf4']
+    PATH = "~/Desktop/Swarm/Swarmskin/data/"       
+
+    drone_list = []
+    for name in cf_names:
+        drone = swarmlib.Drone(name)
+        drone_list.append(drone)
+
+    # landing pads init
+    lp_list = []
+    for lp_name in lp_names:
+        lp_list.append( swarmlib.Mocap_object(lp_name) )
+
+    # landing_velocity = random.choice([13,22]) #13,22
+    landing_velocity = 13
+    # landing_velocity = 22
+    # landing_velocity = 30
+    print "landing_velocity", landing_velocity
+
+
+    # flight to landing positions
+    # l=0.3; global_goal_poses = [ [ 0.0, l], [ l,   l], [ l,  -l], [ 0.0,-l] ]
+    l=0.3; global_goal_poses = [ lp_list[0].position()[:2], lp_list[1].position()[:2], [ l,  -l], [ 0.0,-l] ]
+    if toFly:
+        # cfs init
+        cf_list = []
+        for cf_name in cf_names:
+            cf = crazyflie.Crazyflie(cf_name, '/vicon/'+cf_name+'/'+cf_name)
+            cf.setParam("commander/enHighLevel", 1)
+            cf.setParam("stabilizer/estimator", 2)  # Use EKF
+            cf.setParam("stabilizer/controller", 2) # Use Mellinger controller
+            time.sleep(0.1)
+            cf_list.append(cf)
+        threads = []
+        for i in range(len(cf_names)):
+            print cf_names[i]
+            threads.append( Thread(target=goto_land_pose, args=(cf_list[i], TakeoffHeight, global_goal_poses[i],)) )
+            threads[-1].start()
+
+
+    if data_recording:
+        try:
+            Subject_name = sys.argv[1]
+        except:
+            print "\nEnter subject's name. For example:"
+            print "rosrun crazyflie_demo swarmskin.py NAME"
+            os.system("rosnode kill /swarmskin")
+            sys.exit(1)
+        pose_recording = Process(target=start_recording)
+        pose_recording.start()
 
 
 
-
-    # time_to_sleep = 1.5
-    # cf.goTo(goal = [.5, .5, 0.0], yaw=-.75*3.14, duration = 2.0, relative = True)
-    # time.sleep(3.0)
-    # for i in range(2):
-    #     cf.goTo(goal = [0.0, -1.0, 0.0], yaw=-1.57, duration = 2.0, relative = True)
-    #     time.sleep(time_to_sleep)
-    #     cf.goTo(goal = [-1.0, 0.0, 0.0], yaw=-1.57, duration = 2.0, relative = True)
-    #     time.sleep(time_to_sleep)
-    #     cf.goTo(goal = [0.0, 1.0, 0.0], yaw=-1.57, duration = 2.0, relative = True)
-    #     time.sleep(time_to_sleep)
-    #     cf.goTo(goal = [1.0, 0.0, 0.0], yaw=-1.57, duration = 2.0, relative = True)
-    #     time.sleep(time_to_sleep)
-    # cf.goTo(goal = [0, 0.0, 0.0], yaw=0, duration = 2.0, relative = False)
-    # time.sleep(3.0)
+    # detect if drones touch landing pads
+    land_detector()
 
 
 
-    # traj1 = uav_trajectory.Trajectory()
-    # traj1.loadcsv("takeoff.csv")
-
-    # traj2 = uav_trajectory.Trajectory()
-    # traj2.loadcsv("figure8.csv")
-
-    # print(traj1.duration)
-
-    # cf.uploadTrajectory(0, 0, traj1)
-    # cf.uploadTrajectory(1, len(traj1.polynomials), traj2)
-
-    # cf.startTrajectory(0, timescale=1.0)
-    # time.sleep(traj1.duration * 2.0)
-
-    # cf.startTrajectory(1, timescale=2.0)
-    # time.sleep(traj2.duration * 2.0)
-
-    # cf.startTrajectory(0, timescale=1.0, reverse=True)
-    # time.sleep(traj1.duration * 1.0)
-
-    # cf.stop()
