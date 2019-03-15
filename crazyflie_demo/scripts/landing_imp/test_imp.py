374f080020dc6ef7d0c4e8d7713822107b9f69a9
RuslanAgishev <agishev_ruslan@mail.ru>
2019-03-15 19:13:15 +0300

    follower

--- crazyflie_demo/scripts/landing_imp/test_imp.py
+++ crazyflie_demo/scripts/landing_imp/test_imp.py
@@ -2,29 +2,14 @@ import numpy as np
 import time
 from scipy.integrate import odeint
 from math import *
-
-def MassSpringDamper(state,t,F, mode='critically_damped'):
-	x = state[0]
-	xd = state[1]
-	if mode=='oscillations':
-		m = 1.0; k = 2; b = 0 # undapmped: oscillations
-	elif mode=='undapmped':
-		m = 1.0; k = 2; b = 2*sqrt(m*k)-2 # underdamped
-	elif mode=='overdamped':
-		m = 1.0; k = 2; b = 2*sqrt(m*k)+2 # overdamped
-	else:
-		m = 1.0; k = 2; b = 2*sqrt(m*k) # critically damped
-	xdd = -(b/m)*xd - (k/m)*x + F/m
-	return [xd, xdd]
+from impedance_modeles import MassSpringDamper
 
 
 y0 = [np.pi - 0.1, 0.0]
-M = 10
-t = np.linspace(0, 10, 101)
+F_external = 10
+t = np.linspace(0, 20, 201)
 mode = 'overdamped'
-sol = odeint(MassSpringDamper, y0, t, args=(M,mode,))
-
-
+sol = odeint(MassSpringDamper, y0, t, args=(F_external, mode,))
 
 
 import matplotlib.pyplot as plt
